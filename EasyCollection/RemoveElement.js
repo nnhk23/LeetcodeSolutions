d7f180f314c3d84aaa209a3300b75e093f1e5d81
Kim Nguyen <nnhk23@gmail.com>
2021-03-11 18:05:28 -0800

    remove element solution

---
+++ EasyCollection/RemoveElement.js
@@ -0,0 +1,13 @@
+var removeElement = function(nums, val) {
+    let i = 0;
+    while(i < nums.length){
+        if(nums[i] === val){
+            nums.splice(i, 1)
+        } else {
+            i++
+        }
+    }
+    return nums.length
+};
+
+removeElement([1,2,3,3,2,3,2], 2)
