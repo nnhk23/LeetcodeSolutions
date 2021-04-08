3539fef3627951d59e2d13462448a56bdab0bd7d
Kim Nguyen <nnhk23@gmail.com>
2021-04-07 17:36:20 -0700

    second solution for number of good pairs

--- HashTable/NumberOfGoodPairs.js
+++ HashTable/NumberOfGoodPairs.js
@@ -1,29 +1,41 @@
 var numIdenticalPairs = function(nums) {
-//     iterate through nums array to create a hashtable.
-//     key: number, value: indices of duplicates in array.
-//     iterate through dict's keys and find count amount of subarrays.
-//     count total of good pairs.
-    let dict = {}
-    for(let i = 0; i < nums.length; i++){
-        if(!dict[nums[i]]){
-            dict[nums[i]] = [i]
-        } else {
-            dict[nums[i]].push(i)
-        }
-    }
+    //     iterate through nums array to create a hashtable.
+    //     key: number, value: indices of duplicates in array.
+    //     iterate through dict's keys and find count amount of subarrays.
+    //     count total of good pairs.
+    // let dict = {}
+    // for(let i = 0; i < nums.length; i++){
+    //     if(!dict[nums[i]]){
+    //         dict[nums[i]] = [i]
+    //     } else {
+    //         dict[nums[i]].push(i)
+    //     }
+    // }
     
-    let totalGoodPairs = 0
-    for(let num in dict){
-        if(dict[num].length > 1){
-            let len = dict[num].length
-            for(let i = 0; i < len; i++){
-                for(let j = i+1; j < len; j++){
-                    totalGoodPairs += 1
-                }
-            }
+    // let totalGoodPairs = 0
+    // for(let num in dict){
+    //     if(dict[num].length > 1){
+    //         let len = dict[num].length
+    //         for(let i = 0; i < len; i++){
+    //             for(let j = i+1; j < len; j++){
+    //                 totalGoodPairs += 1
+    //             }
+    //         }
+    //     }
+    // }
+    // return totalGoodPairs
+    
+    let dict = {}
+    let count = 0
+    for(let num of nums){
+        if(!dict[num]){
+            dict[num] = 1
+        } else {
+            count += dict[num]
+            dict[num]++
         }
     }
-    return totalGoodPairs
+    console.log(count)
 };
 
 numIdenticalPairs([1,2,3,1,1,3])
