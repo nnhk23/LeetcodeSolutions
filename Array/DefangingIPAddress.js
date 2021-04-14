def5ea4ce7e327f9afdb86c2611bd1c83208b6ed
Kim Nguyen <nnhk23@gmail.com>
2021-04-13 17:14:20 -0700

    defanging IP Address solution 3

--- Array/DefangingIPAddress.js
+++ Array/DefangingIPAddress.js
@@ -3,7 +3,16 @@ var defangIPaddr = function(address) {
     // return address.replace(/[^\d]/gi, '[.]')
 
     // solution 2:
-    
+    // let arr = address.split('')
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i] === '.'){
+    //         arr[i] = '[.]'
+    //     }
+    // }
+    // return arr.join('')
+
+    // solution 3:
+    return address.split('.').join('[.]');
 };
 
 defangIPaddr('1.1.1.1')
