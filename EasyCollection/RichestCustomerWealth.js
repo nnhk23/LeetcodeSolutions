57c28636da8060f11642d7de394eea3a0bda04ea
Kim Nguyen <nnhk23@gmail.com>
2021-04-23 14:08:54 -0700

    second solution for richest customer wealth 92.85% performance

--- EasyCollection/RichestCustomerWealth.js
+++ EasyCollection/RichestCustomerWealth.js
@@ -1,20 +1,31 @@
 var maximumWealth = function(accounts) {
-    let dict = {}
+    // let dict = {}
+    // for(let i = 0; i < accounts.length; i++){
+    //     for(let amount of accounts[i]){
+    //         if (dict[i]){
+    //             dict[i] += amount
+    //         } else {
+    //             dict[i] = amount
+    //         }
+    //     }
+    // }
+
+    // let richest = 0
+    // Object.keys(dict).forEach(cus => {
+    //     if(dict[cus] > richest) richest = dict[cus]
+    // })
+    // return richest
+
+    let arr = []
     for(let i = 0; i < accounts.length; i++){
+        let totalMoney = 0
         for(let amount of accounts[i]){
-            if (dict[i]){
-                dict[i] += amount
-            } else {
-                dict[i] = amount
-            }
+            totalMoney += amount
         }
+        arr.push(totalMoney)
     }
 
-    let richest = 0
-    Object.keys(dict).forEach(cus => {
-        if(dict[cus] > richest) richest = dict[cus]
-    })
-    return richest
+    return Math.max(...arr)
 };
 
 maximumWealth([[2,8,7],[7,1,3],[1,9,5]])
